ea1894659b68c878f368371ba76c4b051847f2a4
mylonjones <mylonndj1234@gmail.com>
2022-04-25 19:11:20 -0700

    added media page

--- src/components/media.js
+++ src/components/media.js
@@ -1,9 +1,132 @@
 import React from 'react'
+import axios from 'axios'
+import sampleVideos from '../videodata'
 
-export default function Media() {
-  return (
-    <div>
-      More
-    </div>
-  )
+const key = process.env.REACT_APP_API_YOUTUBE
+
+class Media extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      videos: [],
+      currentVideo: null,
+      query: ''
+    }
+
+    this.changeHandler = this.changeHandler.bind(this)
+    this.searchHandler = this.searchHandler.bind(this)
+  }
+
+  componentDidMount() {
+    const snippets = sampleVideos.map((video)=>{
+      return {
+        id: video.id.videoId,
+        description: video.snippet.description,
+        title: video.snippet.title,
+        thumbnails: video.snippet.thumbnails
+      }
+    })
+    this.setState({
+      videos: snippets,
+      currentVideo: snippets[0]
+    })
+  }
+
+  changeHandler(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  searchHandler(e) {
+    e.preventDefault()
+
+    axios({
+      method: 'get',
+      url: 'https://youtube.googleapis.com/youtube/v3/search',
+      params: {
+        key: key,
+        part: 'snippet',
+        channelId: 'UC7EB04U1QGRFEzBOYtqTgLw',
+        q: this.state.query,
+        type: 'video'
+      }
+    })
+    .then((response) => {
+      let snippets = response.data.items
+      snippets = snippets.map((video)=>{
+        return {
+          id: video.id.videoId,
+          description: video.snippet.description,
+          title: video.snippet.title,
+          thumbnails: video.snippet.thumbnails
+        }
+      })
+
+      this.setState({
+        videos: snippets,
+        currentVideo: snippets[0]
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+
+    this.setState({
+      query: ''
+    })
+  }
+
+  handleVideoClick(video) {
+    this.setState({
+      currentVideo: video
+    })
+  }
+
+  render() {
+
+    return (
+      <div className='mediaContainer'>
+
+        <div className='mediaDisplay'>
+          <div className='mediaHeader'>
+            <div className='bold'>Welcome to our media page</div>
+            <div>Check out our most recent services or search for something specific</div>
+          </div>
+          <div className='search' >
+            <form onSubmit={this.searchHandler} >
+              <label>
+                <input type='text' autoComplete='off' name='query' value={this.state.query} onChange={this.changeHandler}  />
+              </label>
+              <input type='submit' value='search' />
+            </form>
+          </div>
+          <div className='videoContainer' >
+            <iframe
+              className='video'
+              title='exercise'
+              src={`//www.youtube.com/embed/${this.state.currentVideo && this.state.currentVideo.id}`}
+            ></iframe>
+          </div>
+        </div>
+        <div className='snippetDisplay'>
+          {this.state.videos.map((video, index)=>{
+            return(<div
+              key={index}
+              className='snippetContainer'
+              onClick={()=>{this.handleVideoClick(video)}} >
+              <img
+                className='snippet'
+                src={video.thumbnails.default.url}
+                alt={video.title} />
+            </div>)
+          })}
+        </div>
+      </div>
+    )
+  }
 }
+
+export default Media
---
+++ src/videodata.js
@@ -0,0 +1,172 @@
+export default  [
+      {
+          "kind": "youtube#searchResult",
+          "etag": "ziVi6dKiECtEgTuP5W7xXmUiNs0",
+          "id": {
+              "kind": "youtube#video",
+              "videoId": "FXOEKSYoKB4"
+          },
+          "snippet": {
+              "publishedAt": "2019-08-21T21:13:29Z",
+              "channelId": "UC7EB04U1QGRFEzBOYtqTgLw",
+              "title": "Arthur Hernandez Memorial",
+              "description": "",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/FXOEKSYoKB4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/FXOEKSYoKB4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/FXOEKSYoKB4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  }
+              },
+              "channelTitle": "Hope Chapel AV",
+              "liveBroadcastContent": "none",
+              "publishTime": "2019-08-21T21:13:29Z"
+          }
+      },
+      {
+          "kind": "youtube#searchResult",
+          "etag": "tXboXw0TqxEj2CmvLzRBTsDIGfE",
+          "id": {
+              "kind": "youtube#video",
+              "videoId": "VtnWIRk1ioQ"
+          },
+          "snippet": {
+              "publishedAt": "2021-11-18T16:34:36Z",
+              "channelId": "UC7EB04U1QGRFEzBOYtqTgLw",
+              "title": "Hope Chapel AV - November 17, 2021",
+              "description": "Wed Night Service.",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/VtnWIRk1ioQ/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/VtnWIRk1ioQ/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/VtnWIRk1ioQ/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  }
+              },
+              "channelTitle": "Hope Chapel AV",
+              "liveBroadcastContent": "none",
+              "publishTime": "2021-11-18T16:34:36Z"
+          }
+      },
+      {
+          "kind": "youtube#searchResult",
+          "etag": "y9fXXuBKfs-Htyc3Sbd0QY5_zs8",
+          "id": {
+              "kind": "youtube#video",
+              "videoId": "RURgx_qcc9w"
+          },
+          "snippet": {
+              "publishedAt": "2022-01-24T06:13:54Z",
+              "channelId": "UC7EB04U1QGRFEzBOYtqTgLw",
+              "title": "Hope Chapel AV -  January 23, 2022",
+              "description": "8:30 Morning Service.",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/RURgx_qcc9w/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/RURgx_qcc9w/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/RURgx_qcc9w/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  }
+              },
+              "channelTitle": "Hope Chapel AV",
+              "liveBroadcastContent": "none",
+              "publishTime": "2022-01-24T06:13:54Z"
+          }
+      },
+      {
+          "kind": "youtube#searchResult",
+          "etag": "vlkmZ2wKFc2vtyjtprrL1UyQ2n8",
+          "id": {
+              "kind": "youtube#video",
+              "videoId": "ZBiDJv_8jNY"
+          },
+          "snippet": {
+              "publishedAt": "2021-11-14T05:13:07Z",
+              "channelId": "UC7EB04U1QGRFEzBOYtqTgLw",
+              "title": "Hope Chapel AV - November 13, 2021",
+              "description": "Sat. Night of Worship.",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/ZBiDJv_8jNY/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/ZBiDJv_8jNY/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/ZBiDJv_8jNY/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  }
+              },
+              "channelTitle": "Hope Chapel AV",
+              "liveBroadcastContent": "none",
+              "publishTime": "2021-11-14T05:13:07Z"
+          }
+      },
+      {
+          "kind": "youtube#searchResult",
+          "etag": "Z0YkznrJyYYR1NoQUhuNMBxhAEE",
+          "id": {
+              "kind": "youtube#video",
+              "videoId": "od9z4gCEox4"
+          },
+          "snippet": {
+              "publishedAt": "2022-04-21T15:35:19Z",
+              "channelId": "UC7EB04U1QGRFEzBOYtqTgLw",
+              "title": "Hope Chapel AV - Wed Night Service - April 20, 2022",
+              "description": "Wed Night Service.",
+              "thumbnails": {
+                  "default": {
+                      "url": "https://i.ytimg.com/vi/od9z4gCEox4/default.jpg",
+                      "width": 120,
+                      "height": 90
+                  },
+                  "medium": {
+                      "url": "https://i.ytimg.com/vi/od9z4gCEox4/mqdefault.jpg",
+                      "width": 320,
+                      "height": 180
+                  },
+                  "high": {
+                      "url": "https://i.ytimg.com/vi/od9z4gCEox4/hqdefault.jpg",
+                      "width": 480,
+                      "height": 360
+                  }
+              },
+              "channelTitle": "Hope Chapel AV",
+              "liveBroadcastContent": "none",
+              "publishTime": "2022-04-21T15:35:19Z"
+          }
+      }
+  ]
